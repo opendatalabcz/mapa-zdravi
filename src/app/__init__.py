3f39ae422abe4f77adf19ceca67811cd50677dca
Jan Garček <garcejan@fit.cvut.cz>
2022-10-11 17:21:47 +0200

    Add Sqlite db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '46s85h4r5j4htdjhs4h56tjd4jnn8sdtj4d8m4d'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'TODO'
 
-    # db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_NAME}' # TODO setup postresql db
+    db.init_app(app)
 
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
 
+    from .models import DentistsAgeEstimate, Demographics, Students, NRPZS, InsuranceCodes, Insurances, Doctors
+
+    # df = pandas.read_csv(csvfile)
+    # df.to_sql(table_name, conn, if_exists='append', index=False)
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists(f'app/{DB_NAME}'):
+        with app.app_context():
+            db.create_all()
+        print('Database created.')
--- src/app/models.py
+++ src/app/models.py
@@ -1,10 +1,14 @@
 
 from . import db
-from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+# user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # lowercase
+# kraj = relationship("Kraj", back_populates="okresy") # uppercase, ve tride Okres
+# Kraj.okresy = relationship("Okres", back_populates="kraj") # mimo tridu
+
 
 # from sqlalchemy import Column, Integer, Unicode, DateTime, Boolean, Float, Date, ForeignKey, Index, ARRAY, Time
-# from sqlalchemy.orm import relationship
 
 
 class Demographics(db.Model):
@@ -93,5 +97,101 @@ class DentistsAgeEstimate(db.Model):
     last_access = db.Column(db.DateTime(timezone=True), default=func.now())
 
 
-#  TODO Insurance companies
-#  TODO Insurance companies
+class NRPZS(db.Model):
+    __tablename__ = 'NRPZS'
+
+    mistoPoskytovaniId = db.Column(db.Integer)
+    zdravotnickeZarizeniId = db.Column(db.Integer)
+    kod = db.Column(db.Integer, primary_key=True)
+    nazevZarizeni = db.Column(db.Unicode(500))
+    druhZarizeni = db.Column(db.Unicode(500))
+    obec = db.Column(db.Unicode(100))
+    psc = db.Column(db.Integer)
+    ulice = db.Column(db.Unicode(100))
+    cisloDomovniOrientacni = db.Column(db.Unicode(10))
+    kraj = db.Column(db.Unicode(100))
+    krajCode = db.Column(db.String(10))
+    okres = db.Column(db.Unicode(100))
+    okresCode = db.Column(db.String(10))
+    spravniObvod = db.Column(db.Unicode(100))
+    poskytovatelTelefon = db.Column(db.String(100))
+    poskytovatelFax = db.Column(db.String(100))
+    poskytovatelEmail = db.Column(db.String(100))
+    poskytovatelWeb = db.Column(db.String(100))
+    ico = db.Column(db.Integer)
+    typOsoby = db.Column(db.Integer)
+    pravniFormaKod = db.Column(db.Integer)
+    krajCodeSidlo = db.Column(db.String(10))
+    okresCodeSidlo = db.Column(db.String(10))
+    obecSidlo = db.Column(db.Unicode(100))
+    pscSidlo = db.Column(db.Integer)
+    uliceSidlo = db.Column(db.Unicode(100))
+    cisloDomovniOrientacniSidlo = db.Column(db.Integer)
+    oborPece = db.Column(db.Unicode(300))
+    formaPece = db.Column(db.Unicode(500))
+    druhPece = db.Column(db.Unicode(500))
+    odbornyZastupce = db.Column(db.Unicode(200))
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+    last_access = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class InsuranceCodes(db.Model):
+    __tablename__ = 'Zdravotnické kódy'
+
+    code = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(100))
+    rank = db.Column(db.Integer)
+    value = db.Column(db.Float)  # sazba rezie
+
+
+class Insurances(db.Model):
+    __tablename__ = 'Zdravotnické výkony'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    count = db.Column(db.Integer)
+    expertise = db.Column(db.Integer)  # foreign key InsuranceCodes - code
+    facilities_id = db.Column(db.Integer)  # foreign key Ico
+    insurance_company = db.Column(db.String(10))
+    procedure_code = db.Column(db.Integer)
+    region = db.Column(db.Unicode(10))  # cechy, morava, slezsko
+
+
+class Doctors(db.Model):
+    __tablename__ = 'Lékaři'
+
+    # TODO primary key
+
+    # TODO , primary_key=True
+    _id = db.Column(db.Unicode(100), primary_key=True)
+    university = db.Column(db.String(10))
+    graduated_year = db.Column(db.Integer)   # float64
+    lifelong_studies = db.Column(db.Boolean)
+    doctor_name = db.Column(db.Unicode(100))  # object
+    doctor_url = db.Column(db.Unicode(200))   # object
+
+    # TODO rozdelit?
+    medical_specialty = db.Column(db.Unicode(100))
+    private_practice = db.Column(db.Unicode(100))
+    leading_doctor_licence = db.Column(db.Unicode(100))
+    method_of_treatment_licence = db.Column(db.Unicode(100))  # object
+    n_doctor_workplaces = db.Column(db.Unicode(100))  # int64
+    graduated_age_estimate = db.Column(db.Unicode(100))   # int64
+    age_estimate = db.Column(db.Unicode(100))     # float64
+    workplace_name = db.Column(db.Unicode(100))   # object
+    workplace_hospital_ward = db.Column(db.Unicode(100))  # object
+    workplace_address = db.Column(db.Unicode(100))    # object
+    zip_code = db.Column(db.Unicode(100))     # float64
+    street = db.Column(db.Unicode(100))   # object
+    city = db.Column(db.Unicode(100))     # object
+    n_workplaces = db.Column(db.Unicode(100))     # float64
+    n_doctors_in_workplace = db.Column(db.Unicode(100))   # float64
+    workplace_url = db.Column(db.Unicode(100))    # object
+    IC = db.Column(db.Unicode(100))   # object
+    area = db.Column(db.Unicode(100))     # object
+
+    # TODO vyklestit workspaces, jejich obory atd
+    # TODO odstranit camelcase
+    # TODO pridat relations
