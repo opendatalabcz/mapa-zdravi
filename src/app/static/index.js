129a85c55405c70ddfc8b4e3d2fdfdb7e242ee02
Jan Garček <garcejan@fit.cvut.cz>
2022-10-16 15:57:38 +0200

    Add interactive plot

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -1,1 +1,9 @@
 console.log('I am running')
+
+$(document).ready(function() {
+    $( "#setHeight" ).on('load', function() { 
+        var mydiv = $(this).contents().find("div");
+        var h = mydiv.height();
+        $(this).height(h);
+    });
+});
