e8a5544b0ed271590a606712fa2413e5340d8b7b
Jan Garček <garcejan@fit.cvut.cz>
2022-11-19 17:46:22 +0100

    Update inconsistencies

--- src/app/views.py
+++ src/app/views.py
@@ -47,6 +47,13 @@ def map():
     # TODO prediction/history
     # TODO update and visualize them based on the input -> create GET, POST here
 
+    map_kwargs = {
+        'map_title': 'Počet lékařů',
+        'legend_title': 'Počet lékařů / 10 tis. obyv.',
+        'ratio_label': ' / 10 000 obyvatel',
+        'legend_ascending': True
+    }
+
     if request.method == 'POST' and request.form.get('medical_specialty') != 'Všechny obory':
         ms_value = request.form.get('medical_specialty')
         print(ms_value)
@@ -64,7 +71,7 @@ def map():
     doctor_df = pd.DataFrame(doctor_cnt, columns=['district', 'n_doctors'])
 
     # Medical specialty query
-    ms = db.session.query(
+    map_kwargs['ms'] = db.session.query(
         MedicalSpecialty.medical_specialty_name,
         MedicalSpecialty.id
     ).distinct().order_by(MedicalSpecialty.id)
@@ -78,37 +85,26 @@ def map():
 
     demo_df = pd.DataFrame(
         demo_cnt, columns=['district', 'population', 'normalized'])
-    normalized_names = pd.Series(
+    map_kwargs['normalized_names'] = pd.Series(
         demo_df.district.values, index=demo_df.normalized).to_dict()
 
     df = pd.merge(doctor_df, demo_df, on='district')
     df['ratio'] = round(10000 * df.n_doctors / df.population, 2)
     df = df.sort_values('ratio', ascending=False).reset_index(drop=True)
-    ratios = pd.Series(df.ratio.values, index=df.normalized).to_dict()
+    map_kwargs['ratios'] = pd.Series(
+        df.ratio.values, index=df.normalized).to_dict()
 
     # legend labels
     vals = list(df['ratio'].quantile(
         [.2, .3, .5, .65, .75, .90, .95]).values)
-    legend_labels = [round(n, 2) for n in vals] + ['Data nedostupná']
-    print(legend_labels)
-
-    map_kwargs = {
-        'map_title': 'Počet lékařů',
-        'legend_title': 'Počet lékařů / 10 tis. obyv.',
-        'ratio_label': ' / 10 000 obyvatel',
-        'legend_ascending': True
-    }
-
-    print(map_kwargs)
+    map_kwargs['legend_labels'] = [round(n, 2)
+                                   for n in vals] + ['Data nedostupná']
 
     return render_template('map.html',
-                           legend_labels=legend_labels,
-                           medical_specialties=ms,
-                           ratios=ratios,
-                           normalized_names=normalized_names,
+                           **map_kwargs,
                            top5=df.head(),
-                           worst5=df.tail().iloc[::-1],
-                           **map_kwargs)
+                           worst5=df.tail().iloc[::-1]
+                           )
 
 # -------------------------------------------------------------
 
@@ -153,7 +149,8 @@ def age_map():
     # legend labels
     vals = list(district_ages['age_estimate'].quantile(
         [.2, .3, .5, .65, .75, .90, .95]).values)
-    map_kwargs['legend_labels'] = [round(n, 2) for n in vals]
+    map_kwargs['legend_labels'] = [round(n, 2)
+                                   for n in vals] + ['Data nedostupná']
     return map_kwargs
 
 
