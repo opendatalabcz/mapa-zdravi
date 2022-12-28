36ec4d293edc1c87aa903eae110c5f8cd6039c34
Jan Garček <garcejan@fit.cvut.cz>
2022-12-28 16:41:37 +0100

    Dockerization update

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,29 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
+# from flask_migrate import Migrate
 
 from os import path
 from .config import ConfigDB, ConfigDBdev
 
 
 db = SQLAlchemy()
-DB_NAME = 'database.db'
+DB_NAME = "database.db"
 
 
 def create_app(prod=True):
     app = Flask(__name__)
 
     # https://stackoverflow.com/questions/15122312/how-to-import-from-config-file-in-flask
+    print(f"PROD FLAG {prod}")
     if prod:
         app.config.from_object(ConfigDB)
     else:
         app.config.from_object(ConfigDBdev)
 
+    # app.config.from_object(ConfigDB)
     db.init_app(app)
 
-    migrate = Migrate(app, db)
+    # migrate = Migrate(app, db)
 
     from .views import views
 
-    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
     return app
